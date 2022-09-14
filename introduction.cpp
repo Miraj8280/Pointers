24810f60403859c5f60e36dfa3f6d490b7224774
Miraj Asraf <75885581+Miraj8280@users.noreply.github.com>
2022-09-14 20:36:45 +0530

    Create introduction.cpp

---
+++ introduction.cpp
@@ -0,0 +1,37 @@
+/*
+        ------------------------------------------------------------------
+                            Miraj Asraf
+                            miraj_8280
+        ------------------------------------------------------------------
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a = 5;
+	int *p = &a;
+	cout << "a = " << a << endl;
+	cout << "&a = " << &a << endl;
+	cout << "p = " << p << endl;
+	cout << "*p = " << *p << endl;
+	
+	int b = 15;
+	*p = b;
+	cout << "b = " << b << endl;
+	cout << "&b = " << &b << endl;
+	cout << "p = " << p << endl;
+	cout << "*p = " << *p << endl;
+	
+	int c = 20;
+	p = &c;
+	*p = c;
+	cout << "c = " << c << endl;
+	cout << "&c = " << &c << endl;
+	cout << "p = " << p << endl;
+	cout << "*p = " << *p << endl;
+	
+	return 0;
+}
